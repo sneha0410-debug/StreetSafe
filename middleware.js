62dd10b16e4f1b4ef922e1dce3260cf867c10e77
Sneha Lodha <snehalodha03@gmail.com>
2025-11-18 09:59:34 +0530

    Fix middleware authentication error

--- middleware.js
+++ middleware.js
@@ -1,6 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default async function middleware(req, ev) {
+  try {
+    return await clerkMiddleware()(req, ev);
+  } catch (err) {
+    console.error("Clerk middleware error:", err);
+    throw err;
+  }
+}
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],

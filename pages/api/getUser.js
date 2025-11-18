0e37a490a7b7afc0a287750ce69f3b64296705b2
Sneha Lodha <snehalodha03@gmail.com>
2025-11-18 09:16:35 +0530

    Remove old unused API authentication files

--- pages/api/getUser.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from "../../lib/supabaseClient";
-
-export default async function handler(req, res) {
-  const { data } = await supabase.auth.getUser();
-  res.json({ user: data.user });
-}
